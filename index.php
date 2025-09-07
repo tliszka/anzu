5da313a8850c3e48d9efb2547cfb911c0bcf85a2
Tamás <tliszka@gmail.com>
2025-09-07 23:24:25 +0200

    cet

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+// Set the timezone to Central European Time
+date_default_timezone_set('Europe/Budapest');
+
 // Include core files
 require_once __DIR__ . '/src/auth.php';
 require_once __DIR__ . '/config.php';
