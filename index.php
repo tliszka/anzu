c92e549b9a333e8d059559479e707e74883c958e
Tamás <tliszka@gmail.com>
2025-09-07 23:17:49 +0200

    vi

--- index.php
+++ index.php
@@ -50,13 +50,6 @@ try {
 </head>
 <body>
 
-    <div class="video-background">
-        <video autoplay muted loop playsinline>
-            <source src="videos/background.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-    </div>
-
     <div class="container">
         <header class="main-header">
             <div class="logo">Anzu</div>
@@ -91,7 +84,7 @@ try {
         </main>
     </div>
 
-    <div id="auth-modal" class="modal-overlay">
+    <div id="auth-modal" class="modal-overlay" style="display: none;">
         <div class="modal-content">
             <span class="close-modal">&times;</span>
 
